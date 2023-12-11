68de03f881921a9985fd580372d2456b4655a904
Alex <alexhewitt.j@gmail.com>
2023-12-11 13:43:25 -0600

    update packages

---
+++ firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDvrFNtPfJIUc9kGrDMNSVd-V0HA745JHU",
+  authDomain: "notes-app-b0220.firebaseapp.com",
+  projectId: "notes-app-b0220",
+  storageBucket: "notes-app-b0220.appspot.com",
+  messagingSenderId: "489887828523",
+  appId: "1:489887828523:web:851c128f0c4651d827786e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
